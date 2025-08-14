78d1165501c979934a2b1438cda5eb4a474ce0ea
Prashant0304 <prashantpattanashetti5@gmail.com>
2025-08-14 22:26:26 +0530

    Linear search problem

--- LinearSearch/LinearSearch.java
+++ LinearSearch/LinearSearch.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 //Given an array, arr[] of n integers, and an integer element x,
 // find whether element x is present in the array. Return the index of the first occurrence of x in the array,
 // or -1 if it doesn't exist.
